93ae8170474b911afd355bf5e2a5805f5a64806f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 05:57:50 +0000

    Deploy kiyanooshkh/react-dashboard-example to github.com/kiyanooshkh/react-dashboard-example.git:gh-pages

---
+++ precache-manifest.eda6fcb8cccb9d40105bd2e1ef9e8238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82cf719382e62a0c09b5",
+    "url": "/react-dashboard-example/static/css/main.43cbe902.chunk.css"
+  },
+  {
+    "revision": "82cf719382e62a0c09b5",
+    "url": "/react-dashboard-example/static/js/main.82cf7193.chunk.js"
+  },
+  {
+    "revision": "9e88f264d23efb395fff",
+    "url": "/react-dashboard-example/static/js/1.9e88f264.chunk.js"
+  },
+  {
+    "revision": "4dfa13986581e63a5ef3",
+    "url": "/react-dashboard-example/static/js/runtime~main.4dfa1398.js"
+  },
+  {
+    "revision": "da5f44fd4b69ba7618b6c5626fa618fd",
+    "url": "/react-dashboard-example/index.html"
+  }
+];
